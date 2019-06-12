c0453b827f4c1e04d1fbe09c5b3b93d14f53b4f8
John Sokolowski <johndsoko@gmail.com>
2019-06-11 21:34:24 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+'use strict';
+
+$(function() {
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        
+        const listItem = $(this).find('input [name="shopping-list-entry"]');
+        
+        $('ul').append(
+           `<li>
+           <span class="shopping-item">${listItem}</span>
+           <div class="shopping-item-controls">
+             <button class="shopping-item-toggle">
+               <span class="button-label">check</span>
+             </button>
+             <button class="shopping-item-delete">
+               <span class="button-label">delete</span>
+             </button>
+           </div>
+         </li>` 
+        )
+
+    })
+
+    $('h1').hide();
+
+
+
+
+
+
+
+
+});
